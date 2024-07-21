c72bdce7a8047c871ff402213f4e2b5afe2831f4
ameyagidh <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>
2024-07-21 11:42:12 -0400

    comments added

--- src/app/rooms-list/rooms-list.component.ts
+++ src/app/rooms-list/rooms-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { HotelroomsComponent } from '../hotelrooms/hotelrooms.component';
 import { roomlist } from '../hotelrooms/hotelroomsInterface';
 
 @Component({
