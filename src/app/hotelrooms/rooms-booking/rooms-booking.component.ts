feaac4b30920a5df1a8ff977edd7b9973da8038b
ameyagidh <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>
2024-07-26 02:44:32 -0400

    done

--- src/app/hotelrooms/rooms-booking/rooms-booking.component.ts
+++ src/app/hotelrooms/rooms-booking/rooms-booking.component.ts
@@ -1,26 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'hiv-rooms-booking',
   templateUrl: './rooms-booking.component.html',
-  styleUrl: './rooms-booking.component.scss'
+  styleUrls: ['./rooms-booking.component.scss']  // Corrected 'styleUrl' to 'styleUrls'
 })
-export class RoomsBookingComponent implements OnInit{
-
+export class RoomsBookingComponent implements OnInit {
+  
   routeParams: any;
   id: number = 0;
+  id_$!: Observable<number>;
 
-  constructor(private router:ActivatedRoute) 
-  {
-
-  }
+  constructor(private router: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.router.params.subscribe((params)=>{console.log(params["roomid"])
-    this.routeParams = params;      
-    this.id  = params["roomid"]
-    })
-  }
 
+    this.id_$ = this.router.params.pipe(
+      map(params => +params['roomid']) 
+    );
+    console.log("This is observable pipe data");
+    console.log(this.id_$);
+    
+    console.log("This is snapshot Data");
+    console.log(this.router.snapshot.params["roomid"]);  
+
+    this.router.params.subscribe((params) => {
+      console.log(params["roomid"]);
+      this.routeParams = params;      
+      this.id = +params["roomid"];  // Ensuring id is a number
+    });
+  }
 }
